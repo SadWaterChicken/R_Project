f7a8ee7598d56c7d9d1130406ef662f8c0d30550
Kakarot05 <ngonloc@gmail.com>
2026-05-15 17:34:50 +0700

    update inventory, shop but not model npc shop

--- Assets/Dev/Scripts/ShopSystem/ShopDataJson.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-[Serializable]
-public class ShopDataJson
-{
-    public string shopName;
-    public List<ItemData> items = new List<ItemData>();
-}
